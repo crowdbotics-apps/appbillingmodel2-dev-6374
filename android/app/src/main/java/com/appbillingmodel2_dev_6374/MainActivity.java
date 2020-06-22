0cd72aa7f8f494f063a64044becddc8d23ac3021
Crowdbotics <team@crowdbotics.com>
2020-06-22 11:47:39 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/appbillingmodel2_dev_6374/MainActivity.java
@@ -0,0 +1,15 @@
+package com.appbillingmodel2_dev_6374;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "appbillingmodel2_dev_6374";
+    }
+}
